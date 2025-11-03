8b0fc2b324e5157fa07c03f79f36667bd27e12fa
sora_uzu <naruto07170717@gmail.com>
2025-11-03 19:59:16 +0900

    Add thread scaffolding and wire up the Top Inertia page

---
+++ resources/js/Pages/Top.jsx
@@ -0,0 +1,11 @@
+
+import { Head } from "@inertiajs/react";
+
+export default function Top({ user }) {
+    return (
+        <>
+            <Head title="Top" />
+            <h1>AI英会話アプリのトップページです</h1>
+        </>
+    );
+}
