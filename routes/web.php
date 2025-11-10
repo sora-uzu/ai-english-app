4edb35156a1b05a2064cceab306747570d9ce872
sora_uzu <naruto07170717@gmail.com>
2025-11-10 21:22:00 +0900

    [REFACTOR] Move '/top' and '/thread/{id}' routes into the authenticated route group

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/top', [ThreadController::class, 'index'])->name('top');
+    Route::get('/thread/{id}', [ThreadController::class, 'show'])->name('thread.show');
 });
 
 require __DIR__.'/auth.php';
 
-Route::get('/top', [ThreadController::class, 'index'])->name('top');
-
-Route::get('/thread/{id}', [ThreadController::class, 'show'])->name('thread.show');
