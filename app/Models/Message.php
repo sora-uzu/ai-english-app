b792d77b5ecfc223ebbfb1607f9796296fa440ec
sora_uzu <naruto07170717@gmail.com>
2025-11-19 23:22:50 +0900

    [FEAT] Enhance Message and Thread models with fillable properties and relationships

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -2,9 +2,28 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Message extends Model
 {
-    //
+    /** @use HasFactory<\Database\Factories\MessageFactory> */
+    use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'thread_id',
+        'message_en',
+        'message_ja',
+        'sender',
+        'audio_file_path',
+    ];
+
+    public function thread(): BelongsTo
+    {
+        return $this->belongsTo(Thread::class);
+    }
 }
--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,9 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Thread extends Model
 {
     /** @use HasFactory<\Database\Factories\ThreadFactory> */
     use HasFactory;
+
+    /**
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title',
+    ];
+
+    public function messages(): HasMany
+    {
+        return $this->hasMany(Message::class);
+    }
 }
